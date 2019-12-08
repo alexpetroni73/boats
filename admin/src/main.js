933115d457e390a3c9aa43aa0386f809cd656292
Alex Petroni <alexpetroni@gmail.com>
2019-12-08 19:19:14 +0100

    added apollo & vuetify

--- admin/src/main.js
+++ admin/src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import { createProvider } from './vue-apollo'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  apolloProvider: createProvider(),
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
